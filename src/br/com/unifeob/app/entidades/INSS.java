39bacc365819194d17d64dddd30410459d16a149
IvanSimionato <ivansimionato@rocketmail.com>
2013-05-23 12:20:04 -0300

    Adicionando configuracoes do weld

---
+++ src/br/com/unifeob/app/entidades/INSS.java
@@ -0,0 +1,66 @@
+package br.com.unifeob.app.entidades;
+
+public class INSS {
+	
+	private Long id;
+	private Integer anoRereferente;
+	private Float valorLimiteFaixaUm;
+	private Float percentualFaixaUm;
+	private Float valorLimiteFaixaDois;
+	private Float percentualFaixaDois;
+	private Float valorLimiteFaixaTres;
+	private Float percentualFaixaTres;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public Integer getAnoRereferente() {
+		return anoRereferente;
+	}
+	public void setAnoRereferente(Integer anoRereferente) {
+		this.anoRereferente = anoRereferente;
+	}
+	public Float getValorLimiteFaixaUm() {
+		return valorLimiteFaixaUm;
+	}
+	public void setValorLimiteFaixaUm(Float valorLimiteFaixaUm) {
+		this.valorLimiteFaixaUm = valorLimiteFaixaUm;
+	}
+	public Float getPercentualFaixaUm() {
+		return percentualFaixaUm;
+	}
+	public void setPercentualFaixaUm(Float percentualFaixaUm) {
+		this.percentualFaixaUm = percentualFaixaUm;
+	}
+	public Float getValorLimiteFaixaDois() {
+		return valorLimiteFaixaDois;
+	}
+	public void setValorLimiteFaixaDois(Float valorLimiteFaixaDois) {
+		this.valorLimiteFaixaDois = valorLimiteFaixaDois;
+	}
+	public Float getPercentualFaixaDois() {
+		return percentualFaixaDois;
+	}
+	public void setPercentualFaixaDois(Float percentualFaixaDois) {
+		this.percentualFaixaDois = percentualFaixaDois;
+	}
+	public Float getValorLimiteFaixaTres() {
+		return valorLimiteFaixaTres;
+	}
+	public void setValorLimiteFaixaTres(Float valorLimiteFaixaTres) {
+		this.valorLimiteFaixaTres = valorLimiteFaixaTres;
+	}
+	public Float getPercentualFaixaTres() {
+		return percentualFaixaTres;
+	}
+	public void setPercentualFaixaTres(Float percentualFaixaTres) {
+		this.percentualFaixaTres = percentualFaixaTres;
+	}
+	
+	
+	
+
+}
