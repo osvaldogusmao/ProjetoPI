a11fc881c3d535b829ee7d2e23e7e3e7ac961952
IvanSimionato <ivansimionato@rocketmail.com>
2013-06-03 12:45:38 -0300

    Apagar e alterar IRRF adicionados

--- src/br/com/unifeob/app/controller/IRRFController.java
+++ src/br/com/unifeob/app/controller/IRRFController.java
@@ -79,6 +79,51 @@ public class IRRFController extends HttpServlet {
 			dao.salvar(irrf);
 		}
 		
+		if("apagar".equals(logica)){
+			dao.apagar(Long.parseLong(request.getParameter("id")));
+		}
+		
+		if("alterar".equals(logica)){
+			IRRF irrf = dao.recuperarInstancia(Long.parseLong(request.getParameter("id")));
+			request.setAttribute("irrf", irrf);
+			RequestDispatcher rd = request.getRequestDispatcher("/alterar/irrf/index.jsp");
+			rd.forward(request, response);
+		}
+		
+		if("update".equals(logica)){
+			
+			IRRF irrf = new IRRF();
+			
+			irrf.setId(Long.parseLong(request.getParameter("id")));
+			
+			irrf.setAnoReferente(Integer.parseInt(request.getParameter("anoreferencia")));
+			
+			irrf.setValorFaixaUm(Float.parseFloat(request.getParameter("valorFaixaUm")));
+			irrf.setValorLimiteFaixaUm(Float.parseFloat(request.getParameter("valorLimiteFaixaUm")));
+			irrf.setPercentualFaixaUm(Float.parseFloat(request.getParameter("percentualFaixaUm")));
+			irrf.setDeducaoFaixaUm(Float.parseFloat(request.getParameter("deducaoFaixaUm")));
+			
+			irrf.setValorFaixaDois(Float.parseFloat(request.getParameter("valorFaixaDois")));
+			irrf.setValorLimiteFaixaDois(Float.parseFloat(request.getParameter("valorLimiteFaixaDois")));
+			irrf.setPercentualFaixaDois(Float.parseFloat(request.getParameter("percentualFaixaDois")));
+			irrf.setDeducaoFaixaDois(Float.parseFloat(request.getParameter("deducaoFaixaDois")));
+			
+			irrf.setValorFaixaTres(Float.parseFloat(request.getParameter("valorFaixaTres")));
+			irrf.setValorLimiteFaixaTres(Float.parseFloat(request.getParameter("valorLimiteFaixaTres")));
+			irrf.setPercentualFaixaTres(Float.parseFloat(request.getParameter("percentualFaixaTres")));
+			irrf.setDeducaoFaixaTres(Float.parseFloat(request.getParameter("deducaoFaixaTres")));
+			
+			irrf.setValorLimiteFaixaQuatro(Float.parseFloat(request.getParameter("valorLimiteFaixaQuatro")));
+			irrf.setPercentualFaixaQuatro(Float.parseFloat(request.getParameter("percentualFaixaQuatro")));
+			irrf.setDeducaoFaixaQuatro(Float.parseFloat(request.getParameter("deducaoFaixaQuatro")));
+			
+			irrf.setDeducaoPorDependente(Float.parseFloat(request.getParameter("deducaoPorDependente")));
+			
+			dao.alterar(irrf);
+			
+		}
+		
+		
 	}
 
 }
